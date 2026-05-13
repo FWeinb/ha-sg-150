FWeinb/ha-sg-150

commit 248a971444e95ff012a7ca18b375c91ed19d1104

fix lint error

custom_components/sg_150/types.py
@@ -183,7 +183,7 @@ class SG150DeviceCapability(RootModel):
         return getattr(self.root, name)
 
 
-class SG150DeviceType(str, Enum):
+class SG150DeviceType(Enum):
     """API SG150DeviceType."""
 
     PBXPHONE = "PBXPHONE"

commit fa3b4b1db210ea4c7f2a2359fabb2f93091462ab

Use StrEnum to correctly fix the linting issue
Fixes error introduced in 248a971444e95ff012a7ca18b375c91ed19d1104

custom_components/sg_150/types.py
@@ -4,7 +4,7 @@ Types used in the API.
 These are used as return types of various API-Calls
 """
 
-from enum import Enum
+from enum import StrEnum
 from typing import Any, Literal
 
 from pydantic import BaseModel, Field, RootModel
@@ -198,7 +198,7 @@ class SG150DeviceCapability(RootModel):
         return getattr(self.root, name)
 
 
-class SG150DeviceType(Enum):
+class SG150DeviceType(StrEnum):
     """API SG150DeviceType."""
 
     PBXPHONE = "PBXPHONE"